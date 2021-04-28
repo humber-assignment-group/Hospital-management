30c42a9b73e0d5f91ffa7e4e451951eac1ecace9
Sandeep626 <singh.sandeep.0094@gmail.com>
2021-04-28 19:47:30 -0400

    final

---
+++ Models/ContextClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace hospital_management.Models
+{
+    public class ContextClass:DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
